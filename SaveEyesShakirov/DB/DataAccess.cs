503b5132e1a411749831fc00c96a055968e16dce
Human <ainur.hak2004@gmail.com>
2023-02-18 13:59:18 +0300

    Добавьте файлы проекта.

---
+++ SaveEyesShakirov/DB/DataAccess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveEyesShakirov.DB
+{
+    public static class DataAccess
+    {
+        public static List<Agent> GetAgents()
+        {
+            return db.connection.Agent.ToList();
+        }
+        public static List<AgentType> GetAgentTypes()
+        {
+            return db.connection.AgentType.ToList();
+        }
+        public static List<Product> GetProducts()
+        {
+            return db.connection.Product.ToList();
+        }
+        public static void AddAgent(Agent agent)
+        {
+            db.connection.Agent.Add(agent);
+            db.connection.SaveChanges();
+        }
+        public static void AddProduct(Product product)
+        {
+            db.connection.Product.Add(product);
+            db.connection.SaveChanges();
+        }
+    }
+}
---
+++ SaveEyesShakirov/DB/SalesInYear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveEyesShakirov.DB
+{
+    public partial class Agent
+    {
+        public int SalesCount => ProductSale.Count(a => a.SaleDate >= DateTime.Now.AddYears(-1));
+
+        public int Discount
+        {
+            get
+            {
+                var sum = ProductSale.Sum(a => a.ProductCount * a.Product.MinCostForAgent);
+                if (sum < 10_000) return 0;
+                else if (sum < 50_000) return 5;
+                else if (sum < 150_000) return 10;
+                else if (sum < 500_000) return 20;
+                else return 25;
+            }
+
+        }
+    }
+}
